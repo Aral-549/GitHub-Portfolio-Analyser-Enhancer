014cd33e12cc5531ff01502c8f2f7cff881a1381
Aral-549 <maharajas51449@gmail.com>
2026-02-13 21:50:39 +0530

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env': {
-      API_KEY: process.env.API_KEY
-    }
+    'process.env.API_KEY': JSON.stringify(process.env.API_KEY),
+    'process.env.GITHUB_TOKEN': JSON.stringify(process.env.GITHUB_TOKEN)
   }
 });
